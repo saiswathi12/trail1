ee0bdf245393b08de513a8133556ab1a452917de
saiswathi12 <sswathi.devareddy@gmail.com>
2020-06-21 17:02:29 +0530

    commit from madhu

--- src/test/java/sri/sree.java
+++ src/test/java/sri/sree.java
@@ -1,5 +1,11 @@
 package sri;
 
 public class sree {
+	public static void main(String[] args) {
+		System.out.println("ssss");
+		System.out.println("sss");
+		System.out.println("ssssds");
+		System.out.println("sssfcgs");
+	}
 
 }
